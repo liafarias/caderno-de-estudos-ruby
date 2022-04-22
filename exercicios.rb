e963fa523b71ac50ded6ade9d212f9d60897ece3
liafarias <liacarlos@alu.ufc.br>
2022-04-21 23:51:47 -0300

    Curso de Ruby

--- exercicios.rb
+++ exercicios.rb
@@ -150,3 +150,45 @@ puts 'Digite um CPF'
 number = gets.chomp
 result = verificaCPF(number)
 resultEnd = result
+
+######EXERCICIO AULA 5######
+#crie um programa seguindo este paradigma com:
+    #Classes
+        #Esportista
+            #Métodos: competir -> Imprime a mensagem “Participando de uma competição”
+        #Jogador de futebol
+            #Métodos: correr -> Imprime a mensagem “Correndo atrás da bola”
+        #Maratonista
+            #Métodos: correr-> Imprime a mensagem “Percorrendo o circuito”
+#As classes JogadorDeFutebol e Maratonista devem herdar os comportamentos da classe Esportista.
+#No final do programa execute os métodos competir e correr em objetos do tipo JogadorDeFutebol e Maratonista.
+
+class Esportista
+    def competir
+        puts 'Participando de uma competição'
+    end
+end
+
+class JogadorDeFutebol < Esportista
+    def correr
+        puts 'Correndo atrás da bola'
+    end
+end
+
+class Maratonista < Esportista
+    def correr
+        puts 'Percorrendo o circuito'
+    end
+end
+
+puts 'JOGADOR DE FUTEBOL'
+jogadorDeFutebol = JogadorDeFutebol.new
+jogadorDeFutebol.competir
+jogadorDeFutebol.correr
+
+puts ''
+
+puts 'MARATONISTA'
+maratonista = Maratonista.new
+maratonista.competir
+maratonista.correr
